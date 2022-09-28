9d83defb04f8833c5fccbc9e8799bdcadb58a5ca
Bikrom-Roy <bikromroy2001@gmail.com>
2022-09-29 00:48:21 +0600

    accordion add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Blogs from './components/Blogs/Blogs';
 import HealthClub from './components/HealthClub/HealthClub';
 
 function App() {
   return (
     <div className="App">
       <HealthClub></HealthClub>
+      <Blogs></Blogs>
     </div>
   );
 }
